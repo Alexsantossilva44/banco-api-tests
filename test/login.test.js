5b2666aeb305615f63c72c295a18a239da68a636
Alex Silva <alexsantossilva44@gmail.com>
2026-06-19 17:55:21 +0100

    Iniciando o projeto banco-api-tests

---
+++ test/login.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Login', () => {
+  describe('POST /login', () => {
+    it('Deve autenticar o usuário com credenciais válidas', async () => {
+      const response = await request('http://localhost:3000')
+        .post('/login')
+        .set('Content-Type', 'application/json')
+        .send({ username: 'julio.lima', senha: '123456' });
+      // Test implementation
+
+      //console.log('Response Body:', response.body); // Log the response body for debugging
+      //console.log('Response Status:', response.status); // Log the response status for debugging
+
+      expect(response.status).to.equal(200);
+      expect(response.body.token).to.be.a('string');
+    });
+  });
+});
