e65da472f27337549f8db2e82b3573d5a66bd04f
Alex Silva <alexsantossilva44@gmail.com>
2026-06-20 22:02:44 +0100

    Atualizando configuracoes e testes

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const { expect } = require('chai');
+require('dotenv').config();
 
 describe('Transferencias', () => {
   describe('POST /transferencias', () => {
     it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00.', async () => {
       // Capturar o token de autenticação do usuário logado
-      const responseLogin = await request('http://localhost:3000')
+      const responseLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
         .send({
@@ -15,7 +16,7 @@ describe('Transferencias', () => {
 
       const token = responseLogin.body.token;
 
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`) // Adiciona o token de autenticação no cabeçalho
@@ -31,7 +32,7 @@ describe('Transferencias', () => {
 
     it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10,00.', async () => {
       // Capturar o token de autenticação do usuário logado
-      const responseLogin = await request('http://localhost:3000')
+      const responseLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
         .send({
@@ -41,7 +42,7 @@ describe('Transferencias', () => {
 
       const token = responseLogin.body.token;
 
-      const response = await request('http://localhost:3000')
+      const response = await request(process.env.BASE_URL)
         .post('/transferencias')
         .set('Content-Type', 'application/json')
         .set('Authorization', `Bearer ${token}`) // Adiciona o token de autenticação no cabeçalho
